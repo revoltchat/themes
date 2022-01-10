7a98e627dc2fd0ca8e824f8dbbf663b7e1446bbf
Paul Makles <paulmakles@gmail.com>
2022-01-10 15:00:26 +0000

    feat: add tag parsing

--- src/build.js
+++ src/build.js
@@ -9,6 +9,25 @@ await mkdir(OUT_DIR).catch(() => {});
 let list = await themeList();
 let themes = await Promise.all(list.map(async theme => [theme, await loadTheme(theme)]));
 
+// Full theme manifest
+const all_themes = [];
+for (let [ id, theme ] of themes) {
+    const tags = new Set([...theme.tags]);
+    if (theme.variables.light) {
+        tags.add('light');
+    } else {
+        tags.add('dark');
+    }
+    
+    all_themes.push({
+        ...theme,
+        tags: [...tags].slice(0, 10)
+    });
+}
+
+writeFile(`${OUT_DIR}/all.json`, JSON.stringify(all_themes));
+
+// Manifest separated from themes
 let manifest = {
     generated: new Date().toUTCString(),
     themes: {}
@@ -30,4 +49,3 @@ for (let [ id, theme ] of themes) {
 }
 
 writeFile(`${OUT_DIR}/manifest.json`, JSON.stringify(manifest));
-writeFile(`${OUT_DIR}/all.json`, JSON.stringify(themes));
